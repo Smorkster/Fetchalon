d341e0f9222caef40c37c13dc192ff180c6fadff
Smorkster <smorkster@gmail.com>
2023-03-22 10:50:53 +0100

    (230322) GroupFunctions Initial upload

---
+++ Modules/GroupFunctions.psm1
@@ -0,0 +1,13 @@
+﻿<#
+.Synopsis A collection of functions to run for a group object
+.Description A collection of functions to run for a group object
+.ObjectClass Group
+.State Dev
+#>
+
+param ( $culture = "sv-SE" )
+
+$RootDir = ( Get-Item $PSCommandPath ).Directory.Parent.FullName
+Import-LocalizedData -BindingVariable IntmsgTable -UICulture $culture -FileName "$( ( $PSCommandPath.Split( "\" ) | Select-Object -Last 1 ).Split( "." )[0] ).psd1" -BaseDirectory "$RootDir\Localization\$culture\Modules"
+
+#Export-ModuleMember -Function *
