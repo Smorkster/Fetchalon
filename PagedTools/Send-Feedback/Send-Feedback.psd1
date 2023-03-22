a707c26ded2b33d7ce76cfa2ef3b629f354ac02f
Smorkster <smorkster@gmail.com>
2023-03-22 13:53:00 +0100

    (230322) Send-Feedback Initial upload

---
+++ PagedTools/Send-Feedback/Send-Feedback.psd1
@@ -0,0 +1,9 @@
+﻿ConvertFrom-StringData @'
+ContentBtnSendReport = Skicka
+ContentTblScriptTitle = Välj en funktion eller skript i listan, skriv kommentar och klicka sedan på "Skicka"
+StrBackOfficeMailAddress = backoffice@test.com
+StrNotSentToAuthor = Mail inte skickat till författaren
+StrSentToAuthor = Mail skickat till
+StrSMTP = smtprelay.test.com
+StrSubject = Felanmälan / förslag från Fetchalon
+'@
---
+++ PagedTools/Send-Feedback/Send-Feedback.psm1
@@ -0,0 +1,40 @@
+﻿<#
+.Synopsis Send feedback about function/tool
+.Description Send feedback to backoffice about any of the available functions or tools
+.MenuItem Send feedback
+.State Prod
+.Author Smorkster
+#>
+
+Add-Type -AssemblyName PresentationFramework
+$syncHash = $args[0]
+
+###################### Script start
+
+$BindData = [pscustomobject]@{
+	MsgTable = $syncHash.Data.msgTable
+}
+
+$syncHash.Controls.Window.DataContext = $BindData
+
+$syncHash.Controls.BtnSendReport.Add_Click( {
+	if ( $syncHash.Controls.CbFunctionsList.SelectedItem.Author -match "\((?<id>.{4})\)" )
+	{
+		Send-MailMessage -From ( Get-ADUser ( $env:USERNAME.Substring( 6, 4 ) ) -Properties mail ).mail `
+			-To ( Get-ADUser $Matches.id -Properties mail ).mail `
+			-Body "$( $syncHash.Controls.CbFunctionsList.SelectedItem.Name )<br><br>$( $syncHash.Controls.TbText.Text -replace "`n","<br>" )" `
+			-Encoding bigendianunicode `
+			-SmtpServer $syncHash.Data.msgTable.SMTP `
+			-Subject $syncHash.Data.msgTable.StrSubject `
+			-BodyAsHtml
+		$MailSentToAuthor = $true
+	}
+
+	Send-MailMessage -From ( Get-ADUser ( $env:USERNAME.Substring( 6, 4 ) ) -Properties mail ).mail `
+		-To $syncHash.Data.msgTable.StrBackOfficeMailAddress `
+		-Body "$( $syncHash.Controls.CbFunctionsList.SelectedItem.Name )<br><br>$( $syncHash.Controls.TbText.Text -replace "`n","<br>" )<br><br>$( if ( $MailSentToAuthor ) { "$( $syncHash.Data.msgTable.StrSentToAuthor ) $( ( Get-ADUser ( $env:USERNAME.Substring( 6, 4 ) ) -Properties mail ).mail )" } else { $syncHash.Data.msgTable.StrNotSentToAuthor } )" `
+		-Encoding bigendianunicode `
+		-SmtpServer $syncHash.Data.msgTable.SMTP `
+		-Subject $syncHash.Data.msgTable.StrSubject `
+		-BodyAsHtml
+} )
