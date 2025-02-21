7a2013537678cf5554a31d5438f9ef4acd0638f9
Smorkster <smorkster@gmail.com>
2025-02-21 10:26:25 +0100

    (250221) UserPropHandlers Adjusted code for SearchedItem to fit the new object formating Added new settings PHUserOtherAccountStatus for demonstration

--- Modules/PropHandlers/UserPropHandlers.psm1
+++ Modules/PropHandlers/UserPropHandlers.psm1
@@ -15,53 +15,53 @@ $RootDir = ( Get-Item $PSCommandPath ).Directory.Parent.Parent.FullName
 
 Import-LocalizedData -BindingVariable IntMsgTable -UICulture $culture -FileName "$( ( $PSCommandPath.Split( "\" ) | Select-Object -Last 1 ).Split( "." )[0] ).psd1" -BaseDirectory "$RootDir\Localization"
 
-# Handler to open a users homedirectory in explorer
+# Handler to set a disabled account as enabled
 $PHUserAdEnabled = [pscustomobject]@{
 	Code = '
-	$NewPropValue = $SearchedItem.Enabled
+	$NewPropValue = $SearchedItem.AD.Enabled
 
-	if ( $SearchedItem.DistinguishedName -match $PropLocalization.PLUserAdEnabledCodeOrgMatch )
+	if ( $SearchedItem.AD.DistinguishedName -match $PropLocalization.PLUserAdEnabledCodeOrgMatch )
 	{
 		if ( $NewPropValue )
 		{
 			Show-Splash -Text $PropLocalization.PLUserAdEnabledStrAlreadyActive -NoProgressBar -NoTitle
 		}
-		elseif ( $SearchedItem.Name -match $PropLocalization.PLUserAdEnabledCodeSharedAcc )
+		elseif ( $SearchedItem.AD.Name -match $PropLocalization.PLUserAdEnabledCodeSharedAcc )
 		{
-			if ( $SearchedItem.Enabled -eq $false )
+			if ( $SearchedItem.AD.Enabled -eq $false )
 			{
 				Show-MessageBox -Text $PropLocalization.PLUserAdEnabledStrAlreadyActive -NoProgressBar -NoTitle
 			}
-			elseif ( $SearchedItem.LockedOut )
+			elseif ( $SearchedItem.AD.LockedOut )
 			{
-				Unlock-ADAccount -Identity $SearchedItem.DistinguishedName -Confirm:$false
+				Unlock-ADAccount -Identity $SearchedItem.AD.DistinguishedName -Confirm:$false
 			}
 		}
 		else
 		{
-			if ( $SearchedItem.info -match "$( $PropLocalization.PLUserAdEnabledStrDoNotActivate )" )
+			if ( $SearchedItem.AD.info -match "$( $PropLocalization.PLUserAdEnabledStrDoNotActivate )" )
 			{
 				Show-Splash -Text $PropLocalization.PLUserAdEnabledStrAlreadyActive -NoProgressBar -Title $PropLocalization.PLUserAdEnabledMsgDoNotActivateTitle -Duration 3
 			}
-			elseif ( $SearchedItem.accountExpires -ne $null )
+			elseif ( $SearchedItem.AD.accountExpires -ne $null )
 			{
 				try
 				{
-					$Expires = ( Get-ADUser $SearchedItem -Properties accountExpires ).accountExpires
+					$Expires = ( Get-ADUser $SearchedItem.AD.DistinguishedName -Properties accountExpires ).accountExpires
 					if ( [DateTime]::FromFileTime( $Expires ) -lt ( Get-Date ) )
 					{
 						Show-Splash -Text $PropLocalization.PLUserAdEnabledStrAccountValidityExpired -NoProgressBar -Title $PropLocalization.PLUserAdEnabledStrAccountValidityExpiredTitle -Duration 3
 					}
 					else
 					{
-						if ( $SearchedItem.LockedOut )
+						if ( $SearchedItem.AD.LockedOut )
 						{
-							Unlock-ADAccount -Identity $SearchedItem.DistinguishedName -Confirm:$false
+							Unlock-ADAccount -Identity $SearchedItem.AD.DistinguishedName -Confirm:$false
 							$NewPropValue = $true
 						}
-						elseif ( $SearchedItem.Enabled -eq $false )
+						elseif ( $SearchedItem.AD.Enabled -eq $false )
 						{
-							Unlock-ADAccount -Identity $SearchedItem.DistinguishedName -Confirm:$false
+							Unlock-ADAccount -Identity $SearchedItem.AD.DistinguishedName -Confirm:$false
 							$NewPropValue = $true
 						}
 					}
@@ -108,31 +108,31 @@ $PHUserAdMemberOf = [pscustomobject]@{
 # Handler to check account status
 $PHUserOtherAccountStatus = [pscustomobject]@{
 	Code = '
-	$NewPropValue = $SearchedItem.ExtraInfo.Other.AccountStatus
+	$NewPropValue = $SearchedItem.Other.AccountStatus
 
-	if ( $SearchedItem.info -match "$( $PropLocalization.PLUserOtherAccountStatusStrDoNotActivate )" )
+	if ( $SearchedItem.AD.info -match "$( $PropLocalization.PLUserOtherAccountStatusStrDoNotActivate )" )
 	{
 		$NewPropValue = $PropLocalization.PLUserOtherAccountStatusStrUserBlocked
 	}
-	elseif ( $SearchedItem.accountExpires -ne $null )
+	elseif ( $SearchedItem.AD.accountExpires -ne $null )
 	{
-		if ( $SearchedItem.LockedOut )
+		if ( $SearchedItem.AD.LockedOut )
 		{
 			$NewPropValue = $PropLocalization.PLUserOtherAccountStatusStrLocked
 		}
-		elseif ( $SearchedItem.Enabled -eq $false )
+		elseif ( $SearchedItem.AD.Enabled -eq $false )
 		{
 			$NewPropValue = $PropLocalization.PLUserOtherAccountStatusStrUserDisabled
 		}
 
-		if ( $SearchedItem.Description -match $PropLocalization.PLUserOtherAccountStatusCodeREDesc )
+		if ( $SearchedItem.AD.Description -match $PropLocalization.PLUserOtherAccountStatusCodeREDesc )
 		{
-			$NewPropValue = "$NewPropValue $( $SearchedItem.Description )"
+			$NewPropValue = "$NewPropValue $( $SearchedItem.AD.Description )"
 		}
 
 		try
 		{
-			$Expires = ( Get-ADUser $SearchedItem -Properties accountExpires ).accountExpires
+			$Expires = ( Get-ADUser $SearchedItem.AD.DistinguishedName -Properties accountExpires ).accountExpires
 			if ( -not ( $Expires -eq 0 -or $Expires -eq 9223372036854775807 ) )
 			{
 				if ( [DateTime]::FromFileTime( $Expires ) -lt ( Get-Date ) )
@@ -155,6 +155,8 @@ $PHUserOtherAccountStatus = [pscustomobject]@{
 	Description = $IntMsgTable.HDescUserOtherAccountStatus
 	Progress = 0
 	MandatorySource = "Other"
+	Author = "Smorkster (smorkster)"
+	ValidStartDateTime = "2025-02-05 13:30"
 }
 
 Export-ModuleMember -Variable IntMsgTable
